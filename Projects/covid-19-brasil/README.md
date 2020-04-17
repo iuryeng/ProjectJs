4cf55319e0e83c5c0a0298bfc26e64d7028cc366
Iury Coelho <38250160+iuryeng@users.noreply.github.com>
2020-04-17 14:47:28 -0300

    Add files via upload

---
+++ Projects/covid-19-brasil/README.md
@@ -0,0 +1,37 @@
+# covid-19-brasil.github.io
+[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+Copyright 2020 © <a href="https://www.freecodecamp.org/iurycoelho">Iury Coelho</a>.
+
+Acesse <a href="https://covid-19-brasil.github.io/tracker/">Covid-19-Brasil-Info</a>.
+
+
+- Motivação e Resultados
+
+> Devido a situação atual em que nosso país se encontra e os inúmeros sites que divulgam fake news a respeito do número de casos de covid-19, resolvi criar uma aplicação web que coleta dados sobre o corona virus. A aplicação  é atualizada  o mais rápido possível e pretende fornecer dados consistentes em tabela e gráficos. 
+
+> Tentei inovar em uma aplicação que fosse capaz de oferecer dados consistentes o mais rápido possível.
+
+> Estive trabalhando alguns dias para este objetivo!
+
+> O governo brasileiro atualiza as informações do covid-19 no país normalmente ao final da tarde através do boletim oficial diário no site do <a href="https://saude.gov.br/">Ministério da Saúde</a>.
+
+> No boletim não consta o número de casos informados pelas secretarias em tempo hábil. 
+
+> O que acontece é que, por exemplo, depois das 21 hrs do dia atual, se algum site da secretaria de saúde local for notificado, os dados só irão ser divulgados ou confirmados pelo ministério da saúde no fim da tarde do próximo dia;
+
+> Encontrei uma <a href="https://pt.wikipedia.org/wiki/Interface_de_programa%C3%A7%C3%A3o_de_aplica%C3%A7%C3%B5es">API</a> chamada <a href="https://rapidapi.com/astsiatsko/api/coronavirus-monitor/endpoints">Coronavirus Monitor</a> que fornece dados em JSON e captam dados de vários outros sites rápidos de noticia ao redor do Brasil e do mundo.
+
+> Integrei tudo nesta aplicação web.
+
+> Basicamente a app web faz um misto entre informações oficiais do ministério da saúde e também de informações brasileiras e mundiais  coletadas em vários sites de notícias e governos locais;
+
+> Aqui os casos suspeitos, óbitos em análise e casos recuperados do covid-19 no Brasil são coletados diretamente da API mundial e são atualizados rapidamente, já os casos confirmados e óbitos confirmados são comprovados normalmente ao fim da tarde através do boletim oficial, assegurando ou não o que os dados da API e dos sites de notícias mundiais e locais fornecem ao longo do dia.
+
+> Posto isso,  esta aplicação web se torna uma ferramenta poderosa contra a fake news ao mesmo tempo que pode fornecer estimativas dos casos em tempo hábil, apresentando a população e aos cientístas informações seguras a respeito dos indicadores do casos de covid-19 no Brasil.
+
+> Espero ter ajudado de alguma forma. Devemos nos unir em prol de um país melhor e ser ao máximo transparentes e comprometidos com a verdade.
+
+>"O preço de qualquer coisa é a quantidade de vida que você troca por ela."<a href="https://pt.wikipedia.org/wiki/Henry_David_Thoreau"> Henry David Thoreau</a>
+
+Att, Iury Coelho 06/04/2020.
+
---
+++ Projects/covid-19-brasil/tracker/main.js
@@ -0,0 +1,339 @@
+// GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007 - Copyright(c) 2020 Iury Coelho
+//Variáveis Globais  versao 1.0 - Atualizações do Ministério da Saúde
+
+const json_data = ("covid.json");
+const json_data_occurrence = ("occurrence.json");
+const json_data_region = ("casos_regiao.json");
+const xlabels =[];
+const ylabels=[];  
+const yobitos=[];
+const xlabelsOccurrence = [];
+const ylabelsOccurrence = [];
+const labelsRegion = [];
+const labelsRegionCases = [];
+const labelsRegionDeads = [];
+let numEstados = 0;
+let maiorNumObitos = 0;
+let estadoObitos = [];
+let estadoMaiorObtito ="";
+
+
+//Variáveis Globais versao 2.0 - Atualizações com informações mundiais
+
+let totalCases; // total de casos no mundo 
+let totalDeaths; // total de mortes no mundo
+let totalRecovered // numero de casos recuperados
+let newCases; // novos casos no mundo
+let newDeaths; // novas mortes no mundo 
+let statisticDate; //data da ultima atualização dos dados
+let inforCountry=[];  //todas as informações por pais 
+let inforDeathBrazil;
+let inforRecoverdBrazilString;
+let recoveredBrazil;
+let result = []; // guarda o valor do array achando em find inforCountry
+
+let inforCountryActual=[]; //ultimas informações por pais 
+let inforSupectCasesBrazil; //casos supeitos -- dados gerados a partir da api mundial -- no formato N,NNN
+let inforRecoverdBrazil;
+let inforSupectCasesBrazilString;// casos supeitos no formato NNNN 
+let inforCasesBrazil; //casos no Brasil
+let inforNewDeathsBrazil;
+let numMortos;
+let numConfirmados;
+
+
+let affectedCountrys=[]; // array de paises afetados
+
+
+
+
+const URL_PARTICULAR = "https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php";
+const URL_AFFECTED = "https://coronavirus-monitor.p.rapidapi.com/coronavirus/affected.php";
+const URL_WORLD_STATE = "https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php";
+
+const DEF_API =  { // definicoes da api
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+        "x-rapidapi-key": "0341a96610mshfd1d9fba2b8e0a6p1fff43jsne24da21b3b00" 
+    }
+};
+
+//Definição das Funções
+
+/*--------------------------Casos Particulares por pais---------------------------------------*/
+function getDateParticularCountry(){ // funcao para pegar valores de dados por pais
+fetch(URL_PARTICULAR, DEF_API).then(response => response.json().then(data => { // resposta do json 
+	   
+
+	   inforCountry = data.countries_stat;     //atribui a variavel inforCountry as informações colhidas no array stat_by_country
+	   //inforCountryActual =  inforCountry.slice(-1)[0]; // pegar ultimo elemento do array 
+	   result = inforCountry.find( seed => seed.country_name === 'Brazil' ); // procurar pelo Brazil no conjunto de arrays em inforCountry
+           inforCasesBrazil = result.cases;
+           inforDeathBrazil = result.deaths;
+	   inforCasesBrazilString = inforCasesBrazil.replace(/[,]+/g, '');// regex para retirar a ','*/
+	   inforRecoverdBrazil = result.total_recovered;	   
+	   inforDeathBrazilString = inforDeathBrazil.replace(/[,]+/g, '');
+	
+	   inforRecoverdBrazilString = inforRecoverdBrazil.replace(/[,]+/g, '');
+	   recoveredBrazil = parseFloat(inforRecoverdBrazilString);
+	   inforNewDeathsBrazil = parseFloat(inforDeathBrazilString) - numMortos;
+	   inforSupectCasesBrazil = parseFloat(inforCasesBrazilString) - numConfirmados  ;
+	  document.getElementById("num-suspeitos").innerHTML =  inforSupectCasesBrazil;
+	  document.getElementById("obitos-informados").innerHTML =  inforNewDeathsBrazil;
+	  document.getElementById("recuperados").innerHTML =   recoveredBrazil ;
+	}))
+	.catch(err => {
+	    console.log(err);
+	});
+}
+/*--------------------------Paises Afetados---------------------------------------*/
+
+function getDateAffected(){ // funcao para pegar array com paises afetados
+fetch(URL_AFFECTED, DEF_API).then(response => response.json().then(data => {  // resposta do json  
+	    affectedCountrys = data.affected_countries; //atribui a variavel affectedCountrys as informações colhidas no  array affected_countries 
+	}))  
+	.catch(err => {
+	    console.log(err);
+	});
+}
+/*--------------------------Casos no mundo---------------------------------------*/
+function getDateWorld(){
+	fetch(URL_WORLD_STATE, DEF_API).then(response => response.json().then(data => { // resposta do json 
+	    totalCases = data.total_cases; //atribui a variavel totalCases as informações colhidas no array total_cases
+	    totalDeaths = data.total_deaths;
+	    newCases = data.new_cases; //atribui a variavel newCases as informações colhidas no array new_cases
+	    newDeaths = data.new_deaths; //atribui a variavel newDeaths as informações colhidas no array new_deaths
+	    totalRecovered = data.total_recovered; //atribui a variavel Totalrecovered as informações colhidas no array total_recovered
+	    statisticDate = data.statistic_taken_at;    
+	   let dataCerta = moment(statisticDate).subtract(3, 'hours');
+       let dataValida = dataCerta.format('lll');
+       let horaAtualizacao = moment(dataValida).startOf(statisticDate).fromNow();
+
+	    document.getElementById("atualizacao-api").innerHTML = `${horaAtualizacao}`;	       	    
+	}))
+	.catch(err => {
+	    console.log(err);
+	});
+}
+
+
+
+/*-----versão 1.0------------------------------------------------------------------------------*/
+
+$(document).ready(function(){ // populando a tabela de dados a partir do arquivo json
+	$.getJSON("covid.json", function(data){
+		var covid_data = "";
+		$.each(data,function(key, value){
+			covid_data += '<tr>';
+			covid_data += '<td>'+ value.uf + '</td>';
+			covid_data += '<td>'+ value.estado + '</td>';
+			covid_data += '<td>'+ value.casosConfirmados + '</td>';
+			covid_data += '<td>'+ value.obitos + '</td>';
+			covid_data += '</tr>';
+		});
+		$('#covid-table').append(covid_data);
+	});
+});
+
+
+
+async function createChart() { // funcao para construir o chart covid
+	await getDateChart();
+	const ctx = document.getElementById('covidChart').getContext('2d');
+	const myChart = new Chart(ctx, {
+	    type: 'line',
+	    data: {	        
+	        datasets: [{   
+		        label: 'Confirmados',
+		        data: ylabels,
+		        fill:false,
+		        backgroundColor: 'mediumturquoise',
+		        borderColor: 'mediumturquoise',
+		        borderWidth: 2		          
+		    },{
+		        label: 'Óbitos',
+		        data: yobitos,                
+		        type:'line',
+		        fill:false,
+		        backgroundColor: 'lightcoral',
+		        borderColor: 'lightcoral',
+		        borderWidth: 2	
+	        }],
+	        labels:xlabels,
+	      
+	    },
+	    options: {
+	        responsive: true,
+	        maintainAspectRatio: false,
+	        title: {
+            display: true,
+            text: 'Especifição dos casos por UF',
+            fontSize: 25,
+            fontColor:'rgb(52,60,73)'
+        },
+	        layout: {
+	            padding: {
+	                left: 50,
+	                right: 50,
+	                top: 50,
+	                bottom: 0
+                }
+            }
+	    }
+	});
+}
+
+
+
+async function createChartOccurrence(){ // funcao para construir o chart de ocorrencias diarias
+	await getDateOccurrence();
+const ctx = document.getElementById('occurrenceChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: xlabelsOccurrence,
+        datasets: [{
+            label: 'Casos Novos',
+            data: ylabelsOccurrence,
+            backgroundColor:'rgba(54, 162, 235, 1)',
+            borderColor:'rgba(54, 162, 235, 1)'
+          
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        },
+        title: {
+            display: true,
+            text: 'Evolução dos casos informados por dia',
+            fontSize: 25,
+            fontColor:'rgb(52,60,73)'
+        },
+        layout: {
+	        padding: {
+	            left: 50,
+	            right: 50,
+	            top: 40,
+	            bottom: 0
+            }
+        }
+
+    }
+ });
+}
+
+async function createChartRegion(){ // funcao para construir o chart de classificação de casos por região
+	await getDateRegion();
+	const ctx = document.getElementById('regionChart').getContext('2d');
+	const myHorizontalChart = new Chart(ctx, {
+	    type: 'horizontalBar',
+	    data: {	        
+	        datasets: [{   
+		        label: 'Confirmados',
+		        data: labelsRegionCases,
+		        fill:false,
+		        backgroundColor: 'mediumseagreen',
+		        borderColor:'mediumseagreen'		                 
+		    }/*,{
+		        label: 'Óbitos',
+		        data: labelsRegionDeads,                
+		        type:'horizontalBar',
+		        fill:false,
+		        backgroundColor: 'gray',
+		        borderColor: 'gray',
+		        borderWidth: 2	
+	        }*/],
+	        labels:labelsRegion,	      
+	    },
+	    options: {
+		responsive: true,
+	        maintainAspectRatio: false,
+	    	title: {
+	            display: true,
+	            text: 'Classificação dos casos por região',	            
+	            fontSize: 25,
+	            fontColor:'rgb(52,60,73)'
+            }, 
+		    layout: {
+		        padding: {
+		            left: 40,
+		            right: 50,
+		            top: 50,
+		            bottom: 50
+	            }
+	        }
+	    }   	      
+
+	});
+}
+
+
+async function getDateRegion(){ // funcao para capurar do arquivo json os dados do grafico de ocorrencias por regiao
+	const response = await fetch(json_data_region);
+	const data = await response.json();
+	for (var i = 0; i< data.length; i++){
+		labelsRegion.push((data[i]["regiao"]));
+		labelsRegionCases.push((data[i]["casos"]));	
+		labelsRegionDeads.push((data[i]["mortes"]));			
+	}	
+}
+
+
+
+async function getDateOccurrence(){ // funcao para capurar do arquivo json os dados do grafico de ocorrencias por dias epidemologicos
+	const response = await fetch(json_data_occurrence);
+	const data = await response.json();
+	for (var i = 0; i< data.length; i++){
+		xlabelsOccurrence.push((data[i]["dia"]));
+		ylabelsOccurrence.push((data[i]["ocorrencia"]));			
+	}	
+}
+
+
+
+async function getDateChart() { // funcao para capurar do arquivo json os dados do grafico de casos e obtos por estado 
+	const response = await fetch(json_data);
+	const data = await response.json();
+	for (var i = 0; i< data.length; i++){
+			xlabels.push((data[i]["estado"]));
+			ylabels.push((data[i]["casosConfirmados"]));
+			yobitos.push((data[i]["obitos"]));	
+	}	
+	numEstados = xlabels.length;
+	
+    function sum(arr, n) { // funcao para somar um array a partir de um intervalo n 
+	    var sum = 0;
+	    for (var i = 0; i < n; i++) {
+	        sum += arr[i];
+	    }
+	    return sum; 
+	}
+	"use strict";
+	numConfirmados = sum(ylabels,numEstados); // somando numero de casos confirmados
+	numMortos = sum(yobitos,numEstados); // somando numero de obitos 
+	maiorNumObitos = Math.max(...yobitos); //maior numero de mortos -- numero maximo de um array    
+	data.forEach(function(x) { //separando numero de obitos por estado
+		estadoObitos[x.obitos] = x.uf;
+	});
+	estadoMaiorObtito = estadoObitos[maiorNumObitos]; //capturando o estado com maior obito	
+    //document.getElementById("num-confirmado").innerHTML = numConfirmados; //imprimindo numero de confirmados no top menu
+    //document.getElementById("num-obito").innerHTML = numMortos;	//imprimindo numero de mortos no top menu
+    //document.getElementById("maior-letalidade").innerHTML = `${estadoMaiorObtito}: ${maiorNumObitos}`; //imprimindo esatdo com maior letalidade
+}
+
+
+/*-----Ativação das Fuções-------*/
+createChart();//Criação do gráfico 
+createChartOccurrence();//Criação do gráfico 
+createChartRegion();//Criação do gráfico 
+
+getDateParticularCountry();//Pegar dados particular por pais 
+getDateAffected(); // Pegar os paises afetados 
+getDateWorld();  //Pegar estatísticas mundiais 
+
